e1c67f778b59c1396d6f24befdcd6363cff888b1
paulineium <pk2196@nyu.edu>
2023-12-13 21:37:22 -0500

    Assignment 9
    PWA

--- socket-chat-pwa/public/textEditor.js
+++ socket-chat-pwa/public/textEditor.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const textOptionsSelect = document.getElementById('text-options');
+    const boldButton = document.getElementById('bold-button');
+    const italicButton = document.getElementById('italic-button');
     const textarea = document.getElementById('journal-entry');
     const customizationDiv = document.getElementById('customization');
+
+    let fontWeight = 'normal';
+    let fontStyle = 'normal';
 
     function toggleCustomization() {
         if (customizationDiv.style.display === 'none') {
@@ -15,25 +20,33 @@ document.addEventListener('DOMContentLoaded', () => {
         const selectedOption = textOptionsSelect.value;
 
         switch (selectedOption) {
-            case 'arial':
-            case 'times':
-                textarea.style.fontFamily = selectedOption;
-                textarea.style.fontWeight = 'normal';
-                textarea.style.fontStyle = 'normal';
-                break;
-            case 'bold':
-                textarea.style.fontWeight = 'bold';
-                textarea.style.fontStyle = 'normal';
-                break;
-            case 'italic':
-                textarea.style.fontWeight = 'normal';
-                textarea.style.fontStyle = 'italic';
+            case 'Arial':
+            case 'Times New Roman':
+                fontWeight = 'normal';
+                fontStyle = 'normal';
                 break;
             default:
-                // Handle other cases if needed
                 break;
         }
+
+        applyStyles();
     });
+
+    boldButton.addEventListener('click', () => {
+        fontWeight = fontWeight === 'bold' ? 'normal' : 'bold';
+        applyStyles();
+    });
+
+    italicButton.addEventListener('click', () => {
+        fontStyle = fontStyle === 'italic' ? 'normal' : 'italic';
+        applyStyles();
+    });
+
+    function applyStyles() {
+        textarea.style.fontFamily = textOptionsSelect.value;
+        textarea.style.fontWeight = fontWeight;
+        textarea.style.fontStyle = fontStyle;
+    }
 
     // Assign the toggleCustomization function to the click event of the customization toggle
     document.getElementById('customization-toggle').addEventListener('click', toggleCustomization);
